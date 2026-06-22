af21be74b30d063c73a1fd307565c5ebcf2cad41
RafalDigital <rafaldigitall@gmail.com>
2026-06-22 21:27:19 +0700

    Login Area

--- src/App.jsx
+++ src/App.jsx
@@ -1,122 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import {
+  RiGoogleFill,
+} from '@remixicon/react'
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
 
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.jsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          type="button"
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
-
-      <div className="ticks"></div>
-
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
-
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      <div className="w-full md:min-h-dvh min-h-[100vh] bg-zinc-950 relative">
+          <LoginArea/>
+      </div>
     </>
   )
 }
 
-export default App
+function LoginArea() {
+  return (
+    <>
+    <div className='absolute w-full h-full scale-125 bg-linear-to-tr from-zinc-950 via-zinc-400 to-zinc-950 
+            from-[40%] via-[50%] to-[60%]
+            bg-[length:300%_300%] animate-wave z-0 opacity-0'></div>
+      <div className="w-full md:min-h-dvh min-h-[100vh] mx-auto z-20 backdrop-blur-3xl bg-[repeating-linear-gradient(45deg,transparent,transparent_30px,var(--color-zinc-900)_30px,var(--color-zinc-900)_31px)]">
+        <div className='w-fit p-6 h-fit bg-zinc-800 fixed top-1/2 left-1/2 -translate-1/2 flex flex-col justify-center items-center rounded-xl border border-x-zinc-300/15 border-y-zinc-400/15'>
+        <h1 className='text-2xl text-white font-medium mb-4'>Sign In</h1>
+          <button className="bg-zinc-800 hover:bg-zinc-700 text-white cursor-pointer py-2 px-4 rounded-full flex gap-2 border border-x-zinc-300/15 border-y-zinc-400/15 hover:border-zinc-300/40 transition-all group">
+            <RiGoogleFill className='group-hover:scale-110 transition-all'/>
+            Sign in with Google
+          </button>
+        </div>
+      </div>
+    </>
+  )
+}
